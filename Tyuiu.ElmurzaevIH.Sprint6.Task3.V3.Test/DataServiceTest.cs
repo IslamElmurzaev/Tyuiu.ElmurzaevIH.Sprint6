73a1728802b7bdd9fe426f12ad98a7af6498d81b
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-12-04 01:10:28 +0500

    Task3

--- Tyuiu.ElmurzaevIH.Sprint6.Task3.V3.Test/DataServiceTest.cs
+++ Tyuiu.ElmurzaevIH.Sprint6.Task3.V3.Test/DataServiceTest.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.ElmurzaevIH.Sprint6.Task3.V3.Test
             int[,] res = ds.Calculate(matr);
 
             int[,] wait = new int[5, 5] { { -18, -15, 10, -2, 12 }, { -18, -10, 33, -20, 32 }, { 14, -9, -14, 12, -18 }, { -19, 5, -3, 28, -17 }, { -14, 21, 23, -13, 11 } };
-            CollectionAssert.AreEqual(wait, res)
+            CollectionAssert.AreEqual(wait, res);
         }
     }
 }
